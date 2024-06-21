f711c2257cda01d458d7c137e2afa5c4544af066
zy <yunstv@163.com>
2024-06-21 19:35:24 +0800

    fix: resolves repetitive rendering animation overloads

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -13,13 +13,11 @@ export const ElementInViewport: FC<ElementInViewportProps> = ({
   ...props
 }) => {
   const viewRef = React.useRef(null);
-  const [animateEnd, setAnimateEnd] = React.useState<Boolean>(false);
   React.useEffect(() => {
     if (!viewRef.current) return;
     const observeCallback = (element: HTMLElement, unobserve: any) => {
       animateCSS(element, animation).then(() => {
         unobserve && unobserve();
-        setAnimateEnd(true);
       });
     };
     const observe = elementObserver(
@@ -32,7 +30,6 @@ export const ElementInViewport: FC<ElementInViewportProps> = ({
       observe.end();
     };
   }, [animation, observeOptions]);
-  if (!isWrap && animateEnd) return children;
   return (
     <div ref={viewRef} className={className} {...props}>
       {children}
